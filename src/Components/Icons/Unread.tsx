df08e0914757df9f351ae48cbe8ba22e385bca7d
temple69 <utemple169@gmail.com>
2023-10-10 07:04:52 +0100

    folder that contains images used in project

---
+++ src/Components/Icons/Unread.tsx
@@ -0,0 +1,16 @@
+const Unread = () => {
+  return (
+    <svg
+      className="inline ml-2"
+      xmlns="http://www.w3.org/2000/svg"
+      width="8"
+      height="8"
+      viewBox="0 0 8 8"
+      fill="none"
+    >
+      <circle cx="4" cy="4" r="4" fill="#F65552" />
+    </svg>
+  );
+};
+
+export default Unread;
