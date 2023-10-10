e45909f367035356540e15cad172324dd7a89c4a
temple69 <utemple169@gmail.com>
2023-10-10 07:10:44 +0100

    main file

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Notifications from "./Components/Notifications";
+function App() {
+  return (
+    <main className="bg-[#F7FAFD] flex justify-center items-center min-w-fit  h-fit lg:h-[100vh]">
+      <Notifications />
+    </main>
+  );
+}
+
+export default App;
