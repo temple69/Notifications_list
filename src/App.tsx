11985d06733a960f54d4f20ea584c9179ef49e9d
temple69 <utemple169@gmail.com>
2023-10-12 05:13:43 +0100

    made some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import Notifications from "./Components/Notifications";
+import Notifications from "./Components/Notifications"; //Component
+import { NotificationContextProvider } from "./context/NotificationContext"; //Context
 function App() {
   return (
     <main className="bg-[#F7FAFD] flex justify-center items-center min-w-fit  h-fit lg:h-[100vh]">
-      <Notifications />
+      <NotificationContextProvider>
+        <Notifications />
+      </NotificationContextProvider>
     </main>
   );
 }
