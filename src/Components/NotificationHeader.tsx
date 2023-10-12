b402c16aa85a819ed4858d0497454c0a6c0373ee
temple69 <utemple169@gmail.com>
2023-10-12 05:06:29 +0100

    feat:made unread notifications indicator dynamic

--- src/Components/NotificationHeader.tsx
+++ src/Components/NotificationHeader.tsx
@@ -1,6 +1,11 @@
 import { notificationHeaderType } from "../Types/Types";
 //Component
+import useNotificationContext from "../hooks/useNotificationContext";
 const NotificationHeader = ({ markAllAsRead }: notificationHeaderType) => {
+  //using custom hook
+  const notificationsContext = useNotificationContext();
+  //Destructuring function from context
+  const { unReadNotifications } = notificationsContext;
   return (
     <div className="flex justify-between  items-center">
       <article className="flex gap-2 font-extrabold items-center">
@@ -9,11 +14,11 @@ const NotificationHeader = ({ markAllAsRead }: notificationHeaderType) => {
         </h2>
         <button className="bg-[#0A327B] rounded-md font-extrabold text-[0.8125rem] md:text-base">
           {" "}
-          3
+          {unReadNotifications}
         </button>
       </article>
       <p
-        className="text-[#0A327B] font-medium text-[0.8125rem] md:text-base"
+        className="text-[#0A327B] font-medium text-[0.8125rem] md:text-base cursor-pointer"
         onClick={markAllAsRead}
       >
         Mark all as read
